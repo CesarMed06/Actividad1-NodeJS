e8b637d9c9936dfe07a2aa7c993310b18031e2de
CesarMed06 <cmedsag@alumnos.cei.es>
2025-10-03 14:05:57 +0200

    Inicialización del proyecto (importación de modulos)

---
+++ index.js
@@ -0,0 +1,5 @@
+//Importo los módulos que voy a usar:
+
+const axios = require("axios") //Módulo para hacer peticiones HTTP
+const moment = require("moment") //Módulo para manejar y formatear fechas
+const chalk = require("chalk") //
